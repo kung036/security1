301f67b8cad15f603533fa5c67a57b990db6bc09
kung036 <sinariari3@gmail.com>
2025-01-22 23:35:28 +0900

    feat : 간단한 API 추가
    - user, amdin, manager 등의 API 추가

--- src/main/java/com/cos/security1/controller/IndexController.java
+++ src/main/java/com/cos/security1/controller/IndexController.java
@@ -2,15 +2,47 @@ package com.cos.security1.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller // view 리턴(mustache view)
 public class IndexController {
     // localhost:8080/
     // localhost:8080
     @GetMapping({"", "/"})
-    public String index() {
+    public @ResponseBody String index() {
         // 기본폴더 src/main/resources/
         // 뷰리졸버 설정 : templates (prefix), .mustache(sufifx) 생략 가능
         return "index";
+    }
+
+    @GetMapping("/user")
+    public @ResponseBody String user() {
+        return "user";
+    }
+
+    @GetMapping("/admin")
+    public @ResponseBody String admin() {
+        return "admin";
+    }
+
+    @GetMapping("/manager")
+    public @ResponseBody String manager() {
+        return "manager";
+    }
+
+    // 스프링 시큐리티가 낚아챔
+    @GetMapping("/login")
+    public @ResponseBody String login() {
+        return "login";
+    }
+
+    @GetMapping("/join")
+    public String join() {
+        return "join";
+    }
+
+    @GetMapping("/joinProc")
+    public @ResponseBody String joinProc() {
+        return "회원가입 완료";
     }
 }
