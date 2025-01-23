53a32e90904c36b68325d549a1810527f544128d
kung036 <sinariari3@gmail.com>
2025-01-23 23:52:03 +0900

    feat : 로그인 URL 변경, 암호화 메서드 추가
    - /login -> /login-form
    - 비밀번호 암호화하기 위한 메서드 추가

--- src/main/java/com/cos/security1/config/SecurityConfig.java
+++ src/main/java/com/cos/security1/config/SecurityConfig.java
@@ -6,11 +6,18 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity // 스프링 시큐리티 필터가 스프링 필터체인에 등록됨
 public class SecurityConfig {
+    // 해당 메서드의 리턴되는 오브젝트를 IoC로 등록함
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable);
@@ -23,7 +30,10 @@ public class SecurityConfig {
 
         // 로그인 페이지 설정
 //        http.formLogin(Customizer.withDefaults()); // 디폴트 로그인 페이지
-        http.formLogin(formLogin -> formLogin.loginPage("/login"));
+        http.formLogin(
+                formLogin ->
+                        formLogin.loginPage("/login-forn")
+        );
 
         return http.build();
     }
