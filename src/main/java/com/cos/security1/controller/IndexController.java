0807d31fd7ffb3083d05430875dcd1829cba2ec4
kung036 <sinariari3@gmail.com>
2025-01-23 23:50:56 +0900

    feat : 로그인, 회원가입 API 구현

--- src/main/java/com/cos/security1/controller/IndexController.java
+++ src/main/java/com/cos/security1/controller/IndexController.java
@@ -1,11 +1,20 @@
 package com.cos.security1.controller;
 
+import com.cos.security1.model.User;
+import com.cos.security1.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller // view 리턴(mustache view)
+@RequiredArgsConstructor
 public class IndexController {
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
     // localhost:8080/
     // localhost:8080
     @GetMapping({"", "/"})
@@ -30,19 +39,23 @@ public class IndexController {
         return "manager";
     }
 
-    // 스프링 시큐리티가 낚아챔
-    @GetMapping("/login")
-    public @ResponseBody String login() {
-        return "login";
+    // 스프링 시큐리티가 낚아챔 - SecurityConfig 파일 생성 후 동작 안함
+    @GetMapping("/login-form")
+    public String loginForm() {
+        return "loginForm";
     }
 
-    @GetMapping("/join")
-    public String join() {
-        return "join";
+    @GetMapping("/join-form")
+    public String joinForm() {
+        return "joinForm";
     }
 
-    @GetMapping("/joinProc")
-    public @ResponseBody String joinProc() {
-        return "회원가입 완료";
+    @PostMapping("/join")
+    public String join(User user) {
+        System.out.println(user);
+        user.setRole("ROLE_USER");
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        userRepository.save(user);
+        return "redirect:/login-form";
     }
 }
